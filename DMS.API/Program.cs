cb792264169376e2c09eeff8617212cea546d1f3
Khizir Farrukh <khizirfarrukh@outlook.com>
2023-05-20 14:32:03 +0500

    Separated Builder Service Registrations into separate class

--- DMS.API/Program.cs
+++ DMS.API/Program.cs
@@ -1,3 +1,4 @@
+using DMS.API;
 using DMS.BL.Services;
 using DMS.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -6,22 +7,23 @@ const string _policy = "CorsPolicy";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContextPool<ApplicationDBContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("cs")));
-builder.Services.AddScoped<IStudentRepo, StudentRepo>();
+ServiceRegistration.RegisterServices(builder.Services, builder.Configuration);
+//builder.Services.AddControllers();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddDbContextPool<ApplicationDBContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("cs")));
+//builder.Services.AddScoped<IStudentRepo, StudentRepo>();
 
-builder.Services.AddSwaggerGen();
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy(name: _policy, builder =>
-    {
-        builder.AllowAnyOrigin()
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowAnyOrigin();
-    });
-});
+//builder.Services.AddSwaggerGen();
+//builder.Services.AddCors(opt =>
+//{
+//    opt.AddPolicy(name: _policy, builder =>
+//    {
+//        builder.AllowAnyOrigin()
+//            .AllowAnyHeader()
+//            .AllowAnyMethod()
+//            .AllowAnyOrigin();
+//    });
+//});
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
