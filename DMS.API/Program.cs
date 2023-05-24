f8c316449a7ad57c8b02eb4838e4dd62cbe1423d
Khizir Farrukh <khizirfarrukh@outlook.com>
2023-05-24 15:31:17 +0500

    Fixed Service Extension class

--- DMS.API/Program.cs
+++ DMS.API/Program.cs
@@ -2,28 +2,31 @@ using DMS.API;
 using DMS.BL.Services;
 using DMS.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 const string _policy = "CorsPolicy";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-ServiceRegistration.RegisterServices(builder.Services, builder.Configuration);
-//builder.Services.AddControllers();
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddDbContextPool<ApplicationDBContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("cs")));
-//builder.Services.AddScoped<IStudentRepo, StudentRepo>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContextPool<ApplicationDBContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("cs")));
 
-//builder.Services.AddSwaggerGen();
-//builder.Services.AddCors(opt =>
-//{
-//    opt.AddPolicy(name: _policy, builder =>
-//    {
-//        builder.AllowAnyOrigin()
-//            .AllowAnyHeader()
-//            .AllowAnyMethod()
-//            .AllowAnyOrigin();
-//    });
-//});
+builder.Services.RegisterServices(builder.Configuration);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: "CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyOrigin();
+    });
+});
+
+//ServiceRegistration.RegisterServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
